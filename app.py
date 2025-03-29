f884451bf93baffd14e269b33f8aaf7a825131be
bdvsoftware <bdvsoftware@gmail.com>
2025-03-29 12:13:25 +0100

    directory

--- app.py
+++ app.py
@@ -4,71 +4,99 @@ from matplotlib import pyplot as plt
 import imutils
 import easyocr
 import os
+import csv
 
 # Configurar ruta de Tesseract (ajustar según el sistema)
 # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Windows
 
 def speed_zone(img):
-    # Definir las coordenadas del polígono
-    poly = np.array([[1454,57],[1458,570],[1595,578],[1599,57]], np.int32)
+    # Coordinates for speed display in a 1280x720p img
+    poly = np.array([[984,63],[1014,463],[1255,477],[1252,67]], np.int32)
     poly = poly.reshape((-1, 1, 2))  # Ajustar la forma para OpenCV
 
-    # Crear una máscara negra
     mask = np.zeros_like(img, dtype=np.uint8)
 
-    # Dibujar el polígono en la máscara en blanco
     cv2.fillPoly(mask, [poly], (255, 255, 255))
 
-    # Aplicar la máscara sobre la img
     res = cv2.bitwise_and(img, mask)
 
-    # Encontrar el bounding box del polígono para recortar solo esa región
     x, y, w, h = cv2.boundingRect(poly)
     cut = res[y:y+h, x:x+w]
 
-    # Guardar o mostrar el resultado
     cv2.imwrite("temp/speed_zone.jpg", cut)
     return cut
 
-original = cv2.imread("frames/frame_16.jpg")
-cv2.imwrite("temp/fr_16.png", original)
-img = speed_zone(original)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("temp/gray.png", cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
+debug = False
 
-bfilter = cv2.bilateralFilter(gray, 11, 17, 17) # for noise
-edged = cv2.Canny(bfilter, 0, 200) #Edge detection
-cv2.imwrite("temp/edged.png", cv2.cvtColor(edged, cv2.COLOR_BGR2RGB))
+speed = []
 
-kernel = np.ones((6, 9), np.uint8) 
-dilat = cv2.dilate(edged.copy(), kernel, iterations=2)
-cv2.imwrite("temp/expanded.png", dilat)
+with os.scandir("frames") as files:
+    for file in files:
+        if file.is_file():
+            print(f"Processing file: {file.name}")
 
-keypoints = cv2.findContours(dilat.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-contours = imutils.grab_contours(keypoints)
-contours = sorted(contours, key=cv2.contourArea, reverse = True)[:10]
-locations = []
-for contour in contours:
-    approx = cv2.approxPolyDP(contour, 10, True)
-    if(len(approx) > 4):
-        locations.append(approx)
+            original = cv2.imread(f"frames/{file.name}")
+            if debug:
+                cv2.imwrite("temp/fr_16.png", original)
+            img = speed_zone(original)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            if debug:
+                cv2.imwrite("temp/gray.png", cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
 
-mask = np.zeros(gray.shape, np.uint8)
-for location in locations:
-    new_image = cv2.drawContours(mask, [location], 0, 255, -1)
-    new_image = cv2.bitwise_and(img, img, mask=mask)
-    cv2.imwrite("temp/new_image.png", new_image)
+            bfilter = cv2.bilateralFilter(gray, 11, 17, 17) # for noise
+            edged = cv2.Canny(bfilter, 0, 200) #Edge detection
+            if debug:
+                cv2.imwrite("temp/edged.png", cv2.cvtColor(edged, cv2.COLOR_BGR2RGB))
 
-(x, y) = np.where(mask==255)
-(x1, y1) = (np.min(x), np.min(y))
-(x2, y2) = (np.max(x), np.max(y))
-cropped_image = gray[x1:x2+1, y1:y2+1]
-cv2.imwrite("temp/cropped_image.png", cropped_image)
-cv2.imwrite("temp/new_image_color.png", cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB))
+            kernel = np.ones((6, 9), np.uint8) 
+            dilat = cv2.dilate(edged.copy(), kernel, iterations=2)
+            if debug:
+                cv2.imwrite("temp/expanded.png", dilat)
 
-reader = easyocr.Reader(['en'], gpu=True)
-results = reader.readtext(cropped_image, allowlist='0123456789')
-filtered_results = [res for res in results if res[2] > 0.8]
+            keypoints = cv2.findContours(dilat.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            contours = imutils.grab_contours(keypoints)
+            contours = sorted(contours, key=cv2.contourArea, reverse = True)[:10]
+            locations = []
+            for contour in contours:
+                approx = cv2.approxPolyDP(contour, 10, True)
+                if(len(approx) > 4):
+                    locations.append(approx)
 
-print(filtered_results)
+            mask = np.zeros(gray.shape, np.uint8)
+            for location in locations:
+                new_image = cv2.drawContours(mask, [location], 0, 255, -1)
+                new_image = cv2.bitwise_and(img, img, mask=mask)
+                if debug:
+                    cv2.imwrite("temp/new_image.png", new_image)
+
+            (x, y) = np.where(mask==255)
+            (x1, y1) = (np.min(x), np.min(y))
+            (x2, y2) = (np.max(x), np.max(y))
+            cropped_image = gray[x1:x2+1, y1:y2+1]
+            if debug:
+                cv2.imwrite("temp/cropped_image.png", cropped_image)
+                cv2.imwrite("temp/new_image_color.png", cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB))
+
+            reader = easyocr.Reader(['en'], gpu=True)
+            results = reader.readtext(cropped_image, allowlist='0123456789')
+            filtered_results = [
+                {"value": res[1], "confidence": res[2], "frame": file.name}
+                for res in results if res[2] > 0.8
+            ]
+
+            speed.append(filtered_results)
+
+headers = ["value", "confidence", "frame"]
+
+# Abrimos el archivo en modo escritura
+with open("speed.csv", mode="w", newline="") as file:
+    writer = csv.DictWriter(file, fieldnames=headers)
+    
+    writer.writeheader()
+
+    for result_list in speed:
+        for result in result_list:  
+            writer.writerow(result)
+
+print("FILE GENERATED")
 
